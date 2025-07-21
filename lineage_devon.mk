7d8083846a84fe1ec6d156630f97f6439e7a17b5
George <49768197+thecapibara@users.noreply.github.com>
2025-07-22 00:05:07 +0300

    Vanilla flag

--- lineage_devon.mk
+++ lineage_devon.mk
@@ -13,6 +13,7 @@ $(call inherit-product, device/motorola/devon/device.mk)
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 TARGET_DISABLE_EPPE := true
+USE_GAPPS = FALSE
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_devon
